fb895038daf4bfdaf3674ead709cc26c14cd093c
marmot0814 <32301085+marmot0814@users.noreply.github.com>
2023-10-20 11:06:52 +0800

    [Feat] xbit_vector (#6)
    * [FEAT] Add xbit_vector implementation
    
    * [TEST] Add dibit_vector unittest
    
    * [DOC] Add container module description
    
    * [FIX] Remove useless addgroup tag

---
+++ include/biovoltron/container/all.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * @defgroup container container
+ * @brief The "container" module provides a set of data structures for handling
+ * bioinformatics data.
+ *
+ * This module includes the "xbit_vector" data structure, which is a specialized
+ * container for efficiently storing and manipulating binary data, commonly used
+ * in bioinformatics applications.
+ */
+
+#include <biovoltron/container/xbit_vector.hpp>
